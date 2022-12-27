93ef091d5fdd2445e044ba43f29de4f64a05dd6b
ksw13 <ksw48483@gmail.com>
2022-12-27 12:48:53 +0900

    #14- 게시글 뷰 테스트 내용 업데이트

--- src/test/java/com/example/board/controller/ArticleControllerTest.java
+++ src/test/java/com/example/board/controller/ArticleControllerTest.java
@@ -31,6 +31,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
         //then
     }
@@ -43,7 +44,9 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(model().attributeExists("articles"));
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"));
         //then
     }
     @DisplayName("[view][GET] 게시글 검색 전용 페이지")
